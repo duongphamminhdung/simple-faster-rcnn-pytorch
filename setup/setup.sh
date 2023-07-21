7b71428ab19cc653c521c633064a2e9d47fac026
duongphamminhdung <duongphamminhdung@gmail.com>
2023-07-21 03:18:22 +0000

    add log in setup

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,8 +1,14 @@
+
+echo installing condacolab
 pip install -q condacolab
+
+echo installing nvtop
 apt install nvtop
 
+echo installing anaconda
 python iconda.py
 
+eco installing data
 wget http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCtrainval_06-Nov-2007.tar
 wget http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCtest_06-Nov-2007.tar
 wget http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCdevkit_08-Jun-2007.tar
@@ -11,5 +17,8 @@ tar xvf VOCtrainval_06-Nov-2007.tar
 tar xvf VOCtest_06-Nov-2007.tar
 tar xvf VOCdevkit_08-Jun-2007.tar
 
+echo init conda
 conda init
+
+echo setupconda
 sh setupconda.sh
--- setup/setupconda.sh
+++ setup/setupconda.sh
@@ -1,12 +1,19 @@
 
 # create conda env
+echo creating environment
 conda create --name simp python=3.7
+
+echo activate environment "simp"
 conda activate simp
+
 # install pytorch
+echo installing pytorch
 conda install pytorch==1.5.1 torchvision==0.6.1 cudatoolkit=10.2 -c pytorch
 
 # install other dependancy
+echo installing other dependacy
 pip install visdom scikit-image tqdm fire ipdb matplotlib torchnet
 
 # start visdom
+echo startvisdomy
 nohup python -m visdom.server &
