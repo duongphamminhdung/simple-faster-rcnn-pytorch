14145c81a9c82f7a6f65bc7f6c27add2e691a7d7
duongphamminhdung <duongphamminhdung@gmail.com>
2023-08-01 07:32:17 +0000

    changed setup.sh

--- setup/setup.sh
+++ setup/setup.sh
@@ -8,7 +8,7 @@ apt install nvtop
 echo installing anaconda
 python iconda.py
 
-eco installing data
+echo installing data
 wget http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCtrainval_06-Nov-2007.tar
 wget http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCtest_06-Nov-2007.tar
 wget http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCdevkit_08-Jun-2007.tar
@@ -20,5 +20,3 @@ tar xvf VOCdevkit_08-Jun-2007.tar
 echo init conda
 conda init
 
-echo setupconda
-sh setupconda.sh
