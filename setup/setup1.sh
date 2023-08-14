cb131c1673a6cba62e94e7f0a5372a963a07c558
duongphamminhdung <duongphamminhdung@gmail.com>
2023-08-14 08:38:46 +0000

    changed setup for custom auto

--- setup/setup1.sh
+++ setup/setup1.sh
@@ -11,21 +11,8 @@ apt install nvtop
 echo installing anaconda
 python iconda.py
 
-echo installing data
-# wget https://drive.google.com/file/d/1xtpLsvMBfaw2pj8s4m5lf05i4aB0jmwj/view?usp=drive_link
-# wget http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCtest_06-Nov-2007.tar
-# wget http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCdevkit_08-Jun-2007.tar
-
-# tar xvf VOCtrainval_06-Nov-2007.tar
-# tar xvf VOCtest_06-Nov-2007.tar
-# tar xvf VOCdevkit_08-Jun-2007.tar
-pip install gdown
-gdown https://drive.google.com/uc?id=1xtpLsvMBfaw2pj8s4m5lf05i4aB0jmwj
-unzip VOCdevkit.zip
-
-
 echo init conda
-conda init
+conda init 
 
 echo conda create --name simp python=3.7
 conda create --name simp python=3.7
--- setup/setup2.sh
+++ setup/setup2.sh
@@ -1,3 +1,16 @@
+
+echo installing data
+# wget https://drive.google.com/file/d/1xtpLsvMBfaw2pj8s4m5lf05i4aB0jmwj/view?usp=drive_link
+# wget http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCtest_06-Nov-2007.tar
+# wget http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCdevkit_08-Jun-2007.tar
+
+# tar xvf VOCtrainval_06-Nov-2007.tar
+# tar xvf VOCtest_06-Nov-2007.tar
+# tar xvf VOCdevkit_08-Jun-2007.tar
+pip install gdown
+gdown https://drive.google.com/uc?id=1xtpLsvMBfaw2pj8s4m5lf05i4aB0jmwj
+unzip VOCdevkit.zip
+
 # install pytorch
 echo conda install pytorch==1.5.1 torchvision==0.6.1 cudatoolkit=10.2 -c pytorch
 conda install pytorch==1.5.1 torchvision==0.6.1 cudatoolkit=10.2 -c pytorch
